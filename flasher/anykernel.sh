dbc1f278600ad608ed94db26ea563e69c0b94073
shagbag913 <sh4gbag913@gmail.com>
2019-07-28 21:54:52 -0400

    Add build script and flasher for creating flashable kernel zips
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

---
+++ flasher/anykernel.sh
@@ -0,0 +1,52 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=10x Performance kernel
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=sailfish
+device.name2=marlin
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel install
+dump_boot;
+
+
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.lz4
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=lz4 $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
+fi;
+
+
+ui_print " "; ui_print "Installing boot image...";
+
+
+write_boot;
